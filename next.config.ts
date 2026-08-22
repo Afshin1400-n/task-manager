625d114c6757c6734ed744c4754c6790b1d99f42
Afshin <afshin1993norouzi@outlook.com>
2026-08-22 03:56:50 +0330

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 🔥 اسم مخزن task-manager رو بذار
+  basePath: '/task-manager',
+  assetPrefix: '/task-manager',
 };
 
 export default nextConfig;
